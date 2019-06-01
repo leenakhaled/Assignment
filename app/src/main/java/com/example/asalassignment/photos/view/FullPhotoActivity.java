cb82e4d97cd01604dcbc5b2143c5170dfc6f023b
leenakhaled <leenakhalid393@gmail.com>
2019-06-01 20:31:12 +0300

    - Add fade in for image - Fix the back button in photoActivity

--- app/src/main/java/com/example/asalassignment/photos/view/FullPhotoActivity.java
+++ app/src/main/java/com/example/asalassignment/photos/view/FullPhotoActivity.java
@@ -3,8 +3,9 @@ package com.example.asalassignment.photos.view;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-
 import com.bumptech.glide.Glide;
 import com.example.asalassignment.R;
 
@@ -13,12 +14,10 @@ public class FullPhotoActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fill_photo);
-        ImageView imageView=findViewById(R.id.imageView3);
-
-        Glide.with(this)
-                .load(getIntent().getExtras().getSerializable("imagefill"))
-                .into(imageView);
-   //     imageView.setImageResource( getIntent().getExtras().getString("image"));
+        ImageView imageView = findViewById(R.id.imageView3);
+        Animation myFadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        Glide.with(this).load(getIntent().getExtras().getSerializable("imagefill")).into(imageView);
+        imageView.startAnimation(myFadeInAnimation);
 
 
     }
